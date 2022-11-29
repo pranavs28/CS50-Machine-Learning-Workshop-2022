b716d036f4cffc020c185906b6348f264d89e5f2
pranavs28 <pranav.senthilvel@gmail.com>
2022-11-29 14:58:23 -0500

    2:57 edit

--- Regression/regression.py
+++ Regression/regression.py
@@ -10,4 +10,4 @@ inputs = df.iloc[:,:-1] #remove the sales outcomes from the input data
 
 targets = df.iloc[:,-1:] #get sales outputs
 
-#this is a new edit to my file
+#this is a new edit to my file - 2:57PM
